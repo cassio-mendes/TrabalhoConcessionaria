43600f23f3e6ab605115c26490e6756c9bc7f7d5
cassio-mendes <kassiogithub@gmail.com>
2026-01-05 20:51:23 -0300

    Formatando componentes de input

--- src/Model/DoubleInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class DoubleInvalidoException extends RuntimeException {
-    public DoubleInvalidoException() {
-        super("O atributo precisa ser de tipo double");
-    }
-}
--- src/Model/InteiroInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class InteiroInvalidoException extends RuntimeException {
-    public InteiroInvalidoException() {
-        super("O atributo precisa ser de tipo inteiro");
-    }
-}
