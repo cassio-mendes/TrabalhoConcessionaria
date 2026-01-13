aeaed04b95ad3c4a98ce580008d150f8ca8423ed
beatrizpaivafelix <140505743+beatrizpaivafelix@users.noreply.github.com>
2026-01-13 11:06:06 -0300

    Manipula arquivo sem testes

--- src/Dao/ManipulaArquivo.java
+++ src/Dao/ManipulaArquivo.java
@@ -1,40 +1,74 @@
 package Dao;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class ManipulaArquivo {
+
     private File arquivo;
     private BufferedWriter bw;
     private BufferedReader br;
 
-    public ManipulaArquivo(){
-        this.arquivo = new File("Concessionaria.csv");
+    private static final String NOME_ARQUIVO = "Concessionaria.csv";
+
+    public ManipulaArquivo() {
+        this.arquivo = new File(NOME_ARQUIVO);
     }
 
-    public boolean cadastraCarro(String modelo, double preco, String cor, double consumoCombustivel, int numeroAssentos){
+    public boolean cadastraCarro(String modelo, double preco, String cor,
+                                 double consumoCombustivel, int numeroAssentos) throws IOException {
+
+        bw = new BufferedWriter(new FileWriter(arquivo, true));
+        bw.write(modelo + ";" + preco + ";" + cor + ";" +
+                consumoCombustivel + ";" + numeroAssentos);
+        bw.newLine();
+        bw.close();
 
         return true;
     }
 
-    public boolean cadastraMoto(String modelo, double preco, String cor, double consumoCombustivel, boolean carenagem){
+    public boolean cadastraMoto(String modelo, double preco, String cor,
+                                double consumoCombustivel, boolean carenagem) throws IOException {
+
+        bw = new BufferedWriter(new FileWriter(arquivo, true));
+        bw.write(modelo + ";" + preco + ";" + cor + ";" +
+                consumoCombustivel + ";" + carenagem);
+        bw.newLine();
+        bw.close();
+
         return true;
     }
 
-    public boolean cadastraBicicleta(String acessorio, String modelo, double preco, String cor) {
+    public boolean cadastraBicicleta(String acessorio, String modelo,
+                                     double preco, String cor) throws IOException {
+
+        bw = new BufferedWriter(new FileWriter(arquivo, true));
+        bw.write(modelo + ";" + preco + ";" + cor + ";" + acessorio);
+        bw.newLine();
+        bw.close();
+
         return true;
     }
 
-    public boolean retiraVeiculo(){
+    public boolean retiraVeiculo(String linhaDeletada) throws IOException {
+
+        br = new BufferedReader(new FileReader(arquivo));
+        bw = new BufferedWriter(new FileWriter("temp.csv"));
+
+        String linhaAtual;
+
+        while ((linhaAtual = br.readLine()) != null) {
+            if (!linhaAtual.equals(linhaDeletada)) {
+                bw.write(linhaAtual);
+                bw.newLine();
+            }
+        }
+
+        br.close();
+        bw.close();
+
+        arquivo.delete();
+        new File("temp.csv").renameTo(arquivo);
+
         return true;
     }
-
-    public boolean atualizaVeiculo(){
-        return true;
-    }
-
-    public String[] getListaCompleta() throws IOException { return null; /*Temporário*/}
-
 }
